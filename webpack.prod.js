5fb708804f2053b28f1428b4fd8eb533dfbdae0a
Fatima Najafi <najafifatima762@gmail.com>
2023-08-14 23:11:51 +0430

    Configure the webpack files

---
+++ webpack.prod.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+});
